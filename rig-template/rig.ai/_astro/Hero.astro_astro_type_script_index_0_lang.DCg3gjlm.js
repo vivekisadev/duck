dd77a04630ba998e9cb9bb3e86b09d566ffa24c2
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-07-21 07:08:05 +0000

    docs: implement config (part 66)

---
+++ rig-template/rig.ai/_astro/Hero.astro_astro_type_script_index_0_lang.DCg3gjlm.js
@@ -0,0 +1,1 @@
+import{g as e}from"./posthog.Clu8vfSN.js";const n=await e();if(n){const t=document.getElementById("hero-join-btn");t&&(t.style.display="none")}
---
+++ rig-template/rig.ai/_astro/index.astro_astro_type_script_index_0_lang.CCiO6AmC.js
@@ -0,0 +1,1 @@
+import"./index.astro_astro_type_script_index_1_lang.CmB44lM6.js";
