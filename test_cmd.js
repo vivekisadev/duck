aa4ae34e04a091e89fce96d6358e72e9b63bd156
Vivek Verma <166729635+vivekisadev@users.noreply.github.com>
2026-08-02 01:25:49 +0530

    chore: remove temporary test files

--- test_cmd.js
+++
@@ -1,8 +0,0 @@
-import { Command } from 'commander';
-
-const program = new Command();
-program.command('test1').alias('t1').action(() => {});
-program.command('test2').action(() => {});
-
-const knownCommands = program.commands.map(cmd => cmd.name()).concat(program.commands.flatMap(cmd => cmd.aliases()));
-console.log(knownCommands);
